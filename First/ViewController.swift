5c9c928c6d7a7340f7c7141cc20d59bccc97e9f5
zankoav <zankoav@mail.ru>
2016-03-30 13:48:17 +0300

    zankoav

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+        self.view.backgroundColor = UIColor.blueColor()
     }
 
     override func didReceiveMemoryWarning() {
